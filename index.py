514f1fcfaf7afd4c890d145584cd10e79159bf9a
TajamHeng <oojinheng@gmail.com>
2019-05-02 01:20:27 +0800

    re-work in progress

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from random import randint
 from awesomescraper import AwesomeScraper
 from user import User
 from room import Room
-import json
+import json, datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nothingimportant'
@@ -46,14 +46,17 @@ def connected():
 def disconnect():
     user = users.pop(request.sid,None)
     if user.room in rooms:
-        rooms[user.room].users.pop(request.sid,None)
+        rooms[user.room].users.remove(request.sid)
+        if len(rooms[user.room].users) <= 0:
+            rooms.pop(user.room, None)
+    leave_room(user.room)
     emit('onlineupdate',len(users),room=user.room)
     print('Disconnected: {}'.format(request.sid))
 
 @socketio.on('setname')
 def setname(name):
     users[request.sid].name = name
-    emit('setname',True)
+    emit('setname', name)
     print('Set name {}: {}'.format(name,request.sid))
     
 @socketio.on('createroom')
@@ -62,38 +65,53 @@ def createroom():
     room = Room(room_id)
     room.host_id = request.sid
     rooms[room_id] = room
-    emit('createroom',room_id)
-    print('Create room {}: {}'.format(room_id,request.sid))
+    rooms[room_id].users.append(request.sid)
+    users[request.sid].room = room_id
+    join_room(room_id)
+    emit('createroom', room_id)
+    print('Create room {}: {}'.format(room_id, request.sid))
 
 @socketio.on('joinroom')
 def joinroom(room_id):
     if not room_id in rooms:
-        emit('joinroom',False)
+        emit('joinroom', False)
         return
     rooms[room_id].users.append(request.sid)
     users[request.sid].room = room_id
-    emit('joinroom',True)
-    print('Join room {}: {}'.format(room_id,request.sid))
+    join_room(room_id)
+    emit('joinroom', True)
+    print('Join room {}: {}'.format(room_id, request.sid))
 
 @socketio.on('leaveroom')
 def leaveroom():
     room_id = users[request.sid].room
-    rooms[room_id].users.pop(request.sid,None)
+    rooms[room_id].users.pop(request.sid, None)
     users[request.sid].room = None
-    print('Leave room {}: {}'.format(room_id,request.sid))
+    leave_room(room_id)
+    if len(rooms[room_id].users) <= 0:
+        rooms.pop(room_id, None)
+    emit('leaveroom', room_id)
+    print('Leave room {}: {}'.format(room_id, request.sid))
     
 @socketio.on('chatsend')
 def chatsend(msg):
-    msg = msg_parse(msg)
-    data = {'message':msg,
-            'x':randint(10,90),
-            'y':randint(10,80)}
+    #msg = msg_parse(msg)
+    data = {
+        'message':msg,
+        'x':randint(10,90),
+        'y':randint(10,80),
+        'hash':hash(datetime.datetime.now())
+        }
     room = users[request.sid].room
-    emit('chatreceive',json.dumps(data),room=room)
+    emit('chatreceive',json.dumps(data), room=room)
 
 @app.route('/')
 def index():
-    return render_template('index_.html')
+    return render_template('base.html')
+
+@app.route('/Debug')
+def debug():
+    return render_template('debug.html')
 
 if __name__ == '__main__':
     print('Server served for debug...')
--- static/index.js
+++ static/index.js
@@ -1,29 +1,167 @@
 $(document).ready(function() {
 
+	function effect(position, effect, target = 'body', timeout = 1050) {
+		let element = $('<div></div>')
+			.addClass(effect)
+			.css('left', position.left)
+			.css('top', position.top)
+			.appendTo(target);
+
+		(async () => {
+			await new Promise((resolve, reject) => setTimeout(resolve, timeout));
+			element.remove();
+		})();
+	}
+
+	var vue;
+
+	//var socket = io.connect('https://' + document.domain);
+	var socketio = io.connect('http://' + document.domain + ':20000');
+
+	socketio.on('connect', function() {
+    	socketio.emit('connected');
+	});
+
+	socketio.on('disconnect', function() {
+		console.error('Server disconnected!');
+   		location.reload();
+	});
+
+	socketio.on('setname', function(data) {
+		vue.userdata.nickname = data;
+
+		//Update shows
+		vue.mechanics.show.create = true;
+		vue.mechanics.loading = false;
+
+		//Update display
+		vue.displays.title = 'Hello, ' + vue.userdata.nickname
+		vue.displays.subtitle = 'Enter room ID to join a room.';
+		vue.displays.placeholder = 'Room ID';
+
+		effect({left: '50%', top: '50%'}, 'ripple');
+	});
+
+	socketio.on('joinroom', function(data) {
+		vue.mechanics.loading = false;
+		if (data) {
+			$("#createRoomModal").modal('hide');
+
+			//Update shows
+			vue.mechanics.show.title = false;
+			vue.mechanics.show.input = false;
+			vue.mechanics.show.create = false;
+			vue.mechanics.show.room = true;
+
+			//Update display
+			vue.displays.placeholder = 'Enter message';
+			vue.userdata.roomid = data;
+
+			effect({left: '50%', top: '50%'}, 'ripple');
+		} else {
+			//Prompt user
+			effect({left: '50%', top: '50%'}, 'slash', '#subtitle', 300);
+			vue.displays.subtitle = 'Room not found! Please try another ID.';
+		}
+	});
+
+	socketio.on('chatreceive', function(data) {
+		data = JSON.parse(data);
+		//Parse message
+		vue.mechanics.loading = false;
+		let message = {
+			text: data.message,
+			left: $(window).width() * (data.x / 100),
+			top: $(window).height() * (data.y / 100),
+			key: data.hash
+		};
+
+		let element = $('<div>' + message.text + '</div>')
+			.addClass('chat')
+			.css('left', message.left + 'px')
+			.css('top', message.top + 'px')
+			.appendTo('body');
+
+		effect ({
+			left: message.left + 'px', 
+			top: message.top + 'px',
+		}, 'ripple');
+		
+		(async () => {
+			await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+			element.remove();
+		})();
+	});
+
+	socketio.on('createroom', function(data) {
+		$("#createRoomModal").modal('hide');
+
+		//Update shows
+		vue.mechanics.show.title = false;
+		vue.mechanics.show.input = false;
+		vue.mechanics.show.create = false;
+		vue.mechanics.loading = false;
+		vue.mechanics.show.room = true;
+
+		//Update display
+		vue.displays.placeholder = 'Enter message';
+		vue.userdata.roomid = data;
+
+	});
+
 	vue = new Vue({
 		delimiters:['{$', '$}'],
 		el: '#vue',
 		data: {
-			title: 'Ripple\'s Diary',
-			subtitle: 'Your message ripples and fades.',
-			page: 0,
-			loading: false,
-			placeholder: 'Your nickname',
-			input_model: '',
-			nickname: ''
+			displays: {
+				title: 'Ripple\'s Diary',
+				subtitle: 'Your message ripples and fades.',
+				placeholder: 'Your nickname'
+			},
+			mechanics: {
+				show: {
+					title: true,
+					input: true,
+					create: false,
+					room: false
+				},
+				loading: false,
+				input: ''
+			},
+			userdata: {
+				nickname: '',
+				roomid: '',
+				images: []
+			},
+			socket: socketio
 		},
 		methods: {
 			submit: function() {
-				if (this.page === 0) {
-					this.setname_loading = true;
-					this.nickname = this.input_model;
-					socket.emit('setname', this.input_model);
-					this.input_model = '';
+				let s = this.mechanics.input;
+				if (s.length !== 0 || s.trim()) {
+					this.mechanics.loading = true;
+					let n = this.userdata.nickname;
+					if (n.length === 0 || !n.trim()) {
+						this.socket.emit('setname', s);
+					} else {
+						this.socket.emit('joinroom', s);
+					}
 				}
-				
+				this.mechanics.input = '';
+			},
+			create:function() {
+				this.mechanics.loading = true;
+				this.socket.emit('createroom');
+			},
+			send: function() {
+				let s = this.mechanics.input;
+				if (s.length !== 0 || s.trim()) {
+					this.mechanics.loading = true;
+					this.socket.emit('chatsend', s);
+					this.mechanics.input = '';
+				}
 			}
 		}
 	})
-
 });
 
